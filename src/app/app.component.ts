4211ca03319d328ef0e5b221294b5897e82a9bd1
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2024-06-18 01:45:13 +0700

    feat: Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [HomeComponent],
+  template: `
+      <main>
+        <header>
+            <img src="https://angular.io/assets/images/logos/angular/angular.svg" width="32" height="32">
+        </header>
+        <section class="content">
+          <app-home></app-home>
+        </section>
+      </main>
+    `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'newbie';
+  title = 'hilhi';
 }
