b958a77fb8e10b8b83a85e3fd38a7f08313fed1f
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2024-06-27 19:49:29 +0700

    feat: added form registration

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -120,4 +120,8 @@ export class HousingService {
   getHousingLocationById(id: number): Housinglocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`suming application for ${firstName} ${lastName} with email: ${email}`);
+  }
 }
